541b0bb723709d50a01f3711c84640f3204659da
durgakiran <gatemefuture@gmail.com>
2017-12-10 09:49:09 +0530

    setup environment variables and db connection

--- README.md
+++ README.md
@@ -26,3 +26,5 @@ Before running the tests make sure you are serving the app via `ng serve`.
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+## START NODE SERVER
+first run `ng build` or build the angular solution and then run `node server.js`
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+assert = require('assert');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
-
 // Angular DIST output folder
 app.use(express.static(path.join(__dirname, 'dist')));
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
+MongoClient.connect(process.env.MONGO_URI, function(err, db) {
+    assert.equal(null, err);
+    console.log('successfully connected to database');
+});
 
 //Set Port
-app.listen(3000);
+app.listen(process.env.PORT, function(){
+    console.log('i am listening' + process.env.PORT);
+});
